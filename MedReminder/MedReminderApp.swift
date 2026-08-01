73117583d30f400f22f7ad912952712f5547197a
SevarJafarli <Sevar.Jafarli@abb-bank.az>
2026-08-01 11:36:07 +0400

    Initial Xcode project

---
+++ MedReminder/MedReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedReminderApp.swift
+//  MedReminder
+//
+//  Created by Sevar Jafarli on 01.08.26.
+//
+
+import SwiftUI
+
+@main
+struct MedReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
